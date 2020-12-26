9154255e8f785588439da8f5a856d48b36dbb0fa
WeijieChen2017 <weijiechen1994@gmail.com>
2020-12-26 13:28:58 -0600

    Gibbs recon

--- train_Gibbs_to_PET.py
+++ train_Gibbs_to_PET.py
@@ -18,9 +18,10 @@ img_rows = 256 # image is resampled to this size
 img_cols = 256 # image is resampled to this size
 x_data_folder = 'inv_GIBBS'
 y_data_folder = 'inv_F3'
-weightfile_name = 'weights_PET_PSF_GIBBS.h5'
-model_name = 'model_PET_PSF_GIBBS.json'
-jpgprogressfile_name = 'PET_PSF_GIBBS'
+tag = "_deep4_filter_64"
+weightfile_name = 'weights_PET_PSF_GIBBS'+tag+'.h5'
+model_name = 'model_PET_PSF_GIBBS'+tag+'.json'
+jpgprogressfile_name = 'PET_PSF_GIBBS'+tag
 batch_size = 8 # should be smallish. 1-10
 num_epochs = 25 # should train for at least 100-200 in total
 steps_per_epoch = 20*89 # should be enough to be equal to one whole pass through the dataset
@@ -36,7 +37,7 @@ def train():
     print('-'*50)
     print('Creating and compiling model...')
     print('-'*50)
-    model = Unet.UNetContinuous((img_rows,img_cols,1),start_ch=32,depth=3)
+    model = Unet.UNetContinuous((img_rows,img_cols,1),start_ch=64,depth=4)
     model.compile(optimizer=Adam(lr=1e-4), loss=mean_squared_error, metrics=[mean_squared_error,mean_absolute_error])
     model.summary()
 
