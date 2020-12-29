4b26486fe56c28503aaa3f2e8ccd80eae5ab7777
WeijieChen2017 <weijiechen1994@gmail.com>
2020-12-28 23:07:18 -0600

    Gibbs recon

--- smooth.py
+++ smooth.py
@@ -10,7 +10,7 @@ for nii_name in nii_list:
     print("-----------------------------------------------")
     nii_file = nib.load(nii_name)
     save_file = nib.Nifti1Image(nii_file.get_fdata(), affine=nii_file.affine, header=nii_file.header)
-    smoothed_file = processing.smooth_image(save_file, fwhm=1, mode='nearest')
-    save_name = nii_name[:-4]+"_F1.nii"
+    smoothed_file = processing.smooth_image(save_file, fwhm=3, mode='nearest')
+    save_name = nii_name[:-4]+"_F3.nii"
     nib.save(smoothed_file, save_name)
     print(nii_name)
