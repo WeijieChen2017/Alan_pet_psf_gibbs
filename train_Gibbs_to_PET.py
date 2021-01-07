40b45dafce2db7b924ec2fc08832d0fe68ee8d02
WeijieChen2017 <weijiechen1994@gmail.com>
2021-01-07 00:40:02 -0600

    Gibbs recon

--- train_Gibbs_to_PET.py
+++ train_Gibbs_to_PET.py
@@ -15,7 +15,7 @@ from tensorflow.keras.optimizers import Adam
 from models import Unet
 from utils import NiftiGenerator
 
-para_name = "ex12"
+para_name = "ex13"
 # Data to be written  
 train_para ={  
     "para_name" : para_name,
@@ -23,12 +23,12 @@ train_para ={
     "img_cols" : 256, # image is resampled to this size
     "channel_X" : 5,
     "channel_Y" : 1,
-    "start_ch" : 128,
-    "depth" : 2, 
+    "start_ch" : 64,
+    "depth" : 4, 
     "validation_split" : 0.2,
     "loss" : "l2",
-    "x_data_folder" : 'BRATS_GIBBS',
-    "y_data_folder" : 'BRATS_F3',
+    "x_data_folder" : 'BRATS_GIBBSF3',
+    "y_data_folder" : 'BRATS_GIBBSF3',
     "weightfile_name" : 'weights_'+para_name+'.h5',
     "model_name" : 'model_'+para_name+'.json',
     "save_folder" : './achives/',
