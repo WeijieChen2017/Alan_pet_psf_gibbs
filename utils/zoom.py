e4e9493cc5fd247aa3f82a8b569d0dfd8c094b45
WeijieChen2017 <weijiechen1994@gmail.com>
2021-01-08 11:06:11 -0600

    Gibbs recon

--- utils/zoom.py
+++ utils/zoom.py
@@ -7,8 +7,9 @@ os.system("mkdir 2x")
 nii_list = glob.glob("./*.nii.gz")
 nii_list.sort()
 
-for nii_name in nii_list:
-    print(nii_name)
+for nii_path in nii_list:
+    print(nii_path)
+    nii_name = os.path.basename(nii_path)
     cmd_1 = "3dresample -dxyz 0.586 0.586 1.39 -prefix 2x_"+nii_name+" -inset "+nii_name+" -rmode Cu"
     # cmd_2 = "3dZeropad -I 17 -S 17 p"+idx_str+"+orig"
     cmd_3 = "3dAFNItoNIFTI -prefix 2x_"+nii_name+" 2x_"+nii_name+"+orig"
