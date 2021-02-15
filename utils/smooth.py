e5384103edd43b4a9bd282bb94b4c8d8baaacaaa
WeijieChen2017 <weijiechen1994@gmail.com>
2021-02-15 01:16:29 -0600

    nick

--- utils/smooth.py
+++ utils/smooth.py
@@ -11,6 +11,6 @@ for nii_name in nii_list:
     nii_file = nib.load(nii_name)
     save_file = nib.Nifti1Image(nii_file.get_fdata(), affine=nii_file.affine, header=nii_file.header)
     smoothed_file = processing.smooth_image(save_file, fwhm=3, mode='nearest')
-    save_name = nii_name[:-4]+"_F3.nii"
+    save_name = nii_name[:-7]+"_F3.nii.gz"
     nib.save(smoothed_file, save_name)
     print(nii_name)
