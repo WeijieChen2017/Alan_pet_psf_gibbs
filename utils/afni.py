f3994f03282df42a1f0afa5ea5d5f4b39cbfc636
WeijieChen2017 <weijiechen1994@gmail.com>
2021-02-15 10:51:58 -0600

    nick

--- utils/afni.py
+++ utils/afni.py
@@ -6,7 +6,7 @@ idx_list = ["001", "002", "005", "010", "019", "041", "046", "053", "057", "058"
 for idx_str in idx_list:
 	# idx_str = "{0:0>3}".format(idx+1)
 	print(idx_str)
-	cmd_1 = "3dresample -dxyz 1.172 1.172 1.39 -prefix pet"+idx_str+" -inset sCT_"+idx_str+"_PET.nii.gz -rmode Cu"
+	cmd_1 = "3dresample -dxyz 1.172 1.172 1.07 -prefix pet"+idx_str+" -inset sCT_"+idx_str+"_PET.nii.gz -rmode Cu"
 	# cmd_2 = "3dZeropad -I 17 -S 17 p"+idx_str+"+orig"
 	cmd_3 = "3dAFNItoNIFTI -prefix pet"+idx_str+" pet"+idx_str+"+orig"
 	# cmd_4 = "rm -f zeropad+orig.BRIK"
