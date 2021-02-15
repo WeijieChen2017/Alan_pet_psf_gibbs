f67efb065754c2638f22ec0afeff3331a129ebaf
WeijieChen2017 <weijiechen1994@gmail.com>
2021-02-15 11:51:57 -0600

    nick

--- utils/afni.py
+++ utils/afni.py
@@ -2,18 +2,19 @@ import os
 
 os.system("set AFNI_NIFTI_TYPE_WARN = NO")
 os.system("mkdir PET_2x")
+tag = "sCT_"
 idx_list = ["001", "002", "005", "010", "019", "041", "046", "053", "057", "058", "068", "070"]
 for idx_str in idx_list:
 	# idx_str = "{0:0>3}".format(idx+1)
 	print(idx_str)
-	cmd_1 = "3dresample -dxyz 1.172 1.172 1.065 -prefix pet"+idx_str+" -inset sCT_"+idx_str+"_PET.nii.gz -rmode Cu"
+	cmd_1 = "3dresample -dxyz 1.172 1.172 1.065 -prefix "+tag+idx_str+" -inset "+tag+idx_str+"_PET.nii.gz -rmode Cu"
 	# cmd_2 = "3dZeropad -I 17 -S 17 p"+idx_str+"+orig"
-	cmd_3 = "3dAFNItoNIFTI -prefix pet"+idx_str+" pet"+idx_str+"+orig"
+	cmd_3 = "3dAFNItoNIFTI -prefix "+tag+idx_str+" "+tag+idx_str+"+orig"
 	# cmd_4 = "rm -f zeropad+orig.BRIK"
 	# cmd_5 = "rm -f zeropad+orig.HEAD"
-	cmd_6 = "rm -f pet"+idx_str+"+orig.BRIK"
-	cmd_7 = "rm -f pet"+idx_str+"+orig.HEAD"
-	cmd_8 = "mv pet"+idx_str+".nii ./PET_2x/"
+	cmd_6 = "rm -f "+tag+idx_str+"+orig.BRIK"
+	cmd_7 = "rm -f "+tag+idx_str+"+orig.HEAD"
+	cmd_8 = "mv "+tag+idx_str+".nii ./PET_2x/"
 	# cmd_6 = "mv y"+idx_str+".nii ../inv_RSZP"
 	for cmd in [cmd_1, cmd_3, cmd_6, cmd_7, cmd_8]:
 		print(cmd)
