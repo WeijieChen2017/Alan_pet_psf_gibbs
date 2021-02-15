3138567d0ea4272c44944746c8867613c9712984
WeijieChen2017 <weijiechen1994@gmail.com>
2021-02-15 00:12:19 -0600

    nick

--- utils/collect_nick_data.py
+++ utils/collect_nick_data.py
@@ -11,8 +11,9 @@ PET_list = glob.glob(source_path+"*/"+PET_name+name_type)
 MRI_list.sort()
 PET_list.sort()
 
-for MRI_data in MRI_list:
-	print(MRI_data[33:])
-
 for PET_data in PET_list:
-	print(PET_data[33:])
+    print(PET_data)
+    MRI_data = PET_data[:33]+MRI_name+name_type
+    print(MRI_data)
+    if MRI_data in MRI_list:
+        print("Both exist.")
