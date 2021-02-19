1cff815e13bfb57dd3fd025ce30d875d2d6ddaea
WeijieChen2017 <weijiechen1994@gmail.com>
2021-02-18 19:28:38 -0600

    nick

--- utils/ants.py
+++ utils/ants.py
@@ -6,8 +6,8 @@ for idx in idx_list:
     name_mri = "sCT_"+idx+"_MRI.nii.gz"
     name_pet = "sCT_"+idx+"_PET/nii.gz"
     name_rs = "sCT_"+idx+"_ants.nii.gz"
-    file_mri = ants.image_read(name_mri)
-    file_pet = ants.image_read(name_pet)
-    rs_pet = ants.resample_image_to_target( image=file_pet, target=file_mri, type='bSpline' )
-    ants.image_write( rs_pet, name_rs)
+    # file_mri = ants.image_read(name_mri)
+    # file_pet = ants.image_read(name_pet)
+    # rs_pet = ants.resample_image_to_target( image=file_pet, target=file_mri, type='bSpline' )
+    # ants.image_write( rs_pet, name_rs)
     print(name_rs)
